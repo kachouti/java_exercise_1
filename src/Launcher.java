10702fa200f2d83684a9d1caf287c670f1f8d5fc
kachouti <49548841+kachouti@users.noreply.github.com>
2021-11-29 15:56:58 +0100

    commit Partie4

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class Launcher {
+mport java.util.Scanner;
+public class Launcher {
+
+    private static int fibonacci(int n) {
+        if(n == 0)
+        return 0;
+    else if(n == 1)
+            return 1;
+        else
+            return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+    public static void main(String[] args) {
+        System.out.println("Welcome user");
+        try ( Scanner scanner = new Scanner( System.in ) ) {
+
+            while( true ) {
+                System.out.print( "Enter a variable: " );
+                String var = scanner.nextLine();
+
+                if ( var.equals( "quit" ) ) {
+                    continue;
+                }
+
+
+                else if(var.equals( "fibo" )){
+
+                    System.out.print( "enter a number : " );
+                    int n = scanner.nextInt();
+                    System.out.println(fibonacci(n));
+                }
+
+                else {
+                    System.out.println("Unknown command");
+
+                }
+            }
+
+
+        }
+    }
 }
