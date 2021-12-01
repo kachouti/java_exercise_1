3a9af3ac96017d2d105d5194a400b1205a040ba0
kachouti <49548841+kachouti@users.noreply.github.com>
2021-12-01 09:59:09 +0100

    Update Launcher.java

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,17 +1,75 @@
-mport java.util.Scanner;
-public class Launcher {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+interface Command {
+    String name();
+    boolean run(Scanner scan);
+}
 
-    private static int fibonacci(int n) {
+class   Fibo implements Command {
+    public String name() {
+        Scanner scanner = new Scanner( System.in );
+        System.out.print( "Enter a command: " );
+        String command = scanner.nextLine();
+
+        return command;
+    }
+    public boolean run(Scanner s){
+
+        if(s.equals(name()))
+            return true;
+        else
+            return false;
+
+
+
+    }
+    public  int fibonacci(int n) {
         if(n == 0)
-        return 0;
-    else if(n == 1)
+            return 0;
+        else if(n == 1)
             return 1;
         else
             return fibonacci(n - 1) + fibonacci(n - 2);
     }
+
+}
+class Quit implements Command {
+    public String name() {
+        Scanner scanner = new Scanner( System.in );
+        System.out.print( "Enter a command: " );
+        String command = scanner.nextLine();
+
+        return command;
+    }
+    public boolean run(Scanner s){
+
+        if(s.equals(name()))
+            return true;
+        else
+            return false;
+
+
+
+    }
+
+
+}
+
+
+public class Launcher {
+
     public static void main(String[] args) {
+
+        Command cmd =new Fibo();
         System.out.println("Welcome user");
-        try ( Scanner scanner = new Scanner( System.in ) ) {
+
+       /* ArrayList list=new  ArrayList<>();
+        list.add("fibo");
+        System.out.print( list );*/
+
+
+       try ( Scanne scanne = new Scanner( System.in ) ) {
 
             while( true ) {
                 System.out.print( "Enter a variable: " );
@@ -26,7 +84,7 @@ public class Launcher {
 
                     System.out.print( "enter a number : " );
                     int n = scanner.nextInt();
-                    System.out.println(fibonacci(n));
+                    System.out.println("hh");
                 }
 
                 else {
